6036c4b6fed4d715ba15f7b60acac3b15ece2720
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-26 02:07:54 +0100

    made the sign up form be able to sign into the applicaton with authenticatn

--- src/components/sign-in-component/sign-in.component.jsx
+++ src/components/sign-in-component/sign-in.component.jsx
@@ -5,7 +5,7 @@ import LogoCrown from "../../assets/crown-solid-full.svg";
 import CustomButton from "../custom-button/custom-button.component";
 import CustomInput from "../custom-input-component/custom-input.component";
 
-import { signInWithGoogle } from "../../firebase/firebase-utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase-utils";
 
 import "./sign-in.style.scss";
 
@@ -18,12 +18,19 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   handleChange = (event) => {
+    event.preventDefault();
     const { value, name } = event.target;
     this.setState({ [name]: value });
   };
