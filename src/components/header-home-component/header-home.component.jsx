65e309af9da9782b9ab75281acb8fba69815651c
AGpeter2018 <adenijipeter2018@gmail.com>
2025-10-01 15:11:17 +0100

    added a user sign in profile image

--- src/components/header-home-component/header-home.component.jsx
+++ src/components/header-home-component/header-home.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 import { useSelector, useDispatch } from "react-redux";
 import { setUserAuth } from "../../redux/user/user-action";
@@ -9,9 +9,10 @@ import { selectThemeColor } from "../../redux/theme/theme-selector";
 import { selectHidden } from "../../redux/cart/cart-selector";
 
 import LogoCrown from "../../assets/crown-solid-full.svg";
-import CartIcon from "../../assets/122 shopping-bag.svg";
 import ShoppingIcon from "../shopping-icon-component/shopping-icon.component";
 import DropDown from "../drop-down-component/drop-down.component";
+
+import { FaRegUserCircle } from "react-icons/fa";
 
 import "./header-home.style.scss";
 
@@ -39,6 +40,20 @@ const HeaderHome = () => {
           <Link to="/history" className="option">
             History
           </Link>
+          <div>
+            {currentUser && currentUser.photoURL ? (
+              <img
+                src={currentUser.photoURL}
+                alt=""
+                style={{ width: "30px", height: "25px", borderRadius: "50%" }}
+              />
+            ) : (
+              <FaRegUserCircle
+                className="option"
+                style={{ width: "20px", height: "25px" }}
+              />
+            )}
+          </div>
           {currentUser ? (
             <div className="option" onClick={() => dispatch(setUserAuth())}>
               SignOut
@@ -51,7 +66,7 @@ const HeaderHome = () => {
           <ShoppingIcon />
         </div>
       </div>
-      {hidden ? null : <DropDown />}
+      {currentUser ? hidden ? null : <DropDown /> : <Navigate to="/signIn" />}
     </div>
   );
 };
