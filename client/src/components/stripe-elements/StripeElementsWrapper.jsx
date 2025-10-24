574548e6a8e8983769db997e966ab08e89d9419f
AGpeter2018 <adenijipeter2018@gmail.com>
2025-10-24 05:24:25 +0100

    added a local environment to the client

--- client/src/components/stripe-elements/StripeElementsWrapper.jsx
+++ client/src/components/stripe-elements/StripeElementsWrapper.jsx
@@ -6,9 +6,8 @@ import StripeCheckoutForm from "./StripeCheckoutForm";
 
 // Replace with your Stripe test publishable key
 const stripePromise = loadStripe(
-  process.env.STRIPE_PUBLISHABLE_KEY
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
-console.log("Stripe Publishable Key:", process.env.STRIPE_PUBLISHABLE_KEY);
 
 const StripeElementsWrapper = ({ amount }) => (
   <Elements stripe={stripePromise}>
